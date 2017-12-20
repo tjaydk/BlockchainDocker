32a3ef0442574a7127ed94bb8d257d7df6816811
Dennis Michael <dennis_michael@jubii.dk>
2017-12-20 12:45:28 +0100

    still issues

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from requests_futures.sessions import FuturesSession
+import docker
 import urllib.request
 import os
 import socket
 
+
 # Global variables
 app = Flask(__name__)
+client = docker.from_env()
 onlineUsers = []
+ip = ""
 
 # Get the IP of the Docker Virtual Machine
 def dockerIp():
@@ -13,45 +18,59 @@ def dockerIp():
     host_ip = host_file_obj.readline()
     host_file_obj.close()
     return host_ip
+    
 
 # Ask network for userlist
 # Range will be defined between port 4000 - 4010
 def retrieveList():
-    clientsString = ""
     clientsArray = []
-    port = 4000
-    ip = dockerIp()
-    requestString = "http://" + ip + ":4000/list"
+    clientsString = ""
     
-    for i in range(10):
-        requestString = "http://" + ip + ":" + str(port+i) + "/list"
+    port = 4000
+    ip
+    ownPort = int(socket.gethostname())
+    
+    for i in range(2):
+        currentPort = port+i
+        # Dont ask own port
+        if currentPort == ownPort:
+            continue
+        requestString = "http://" + ip.rstrip('\n') + ":" + str(currentPort) + "/list"
         try:
             req = urllib.request.urlopen(requestString)
-            req.add_header('Content-Type', 'plain/text')
             response = req.read()
             clientsString = str(response)
             # Takes a substring to remove the b' and the ' in beginning and end of response
-            # array = clientsString[2:-1].split(",")
+            array = clientsString[2:-1].split(",")
             # Swaps the user list if the one recieved from the client is bigger
-            #if len(array) > len(clientsArray):
-            #    clientsArray = array
+            if len(array) > len(clientsArray):
+                clientsArray = array
         except urllib.error.URLError as e:
             print(e.reason)
-    # Returns the list of clients and appends self to the list
-    # clientsArray.append(socket.gethostname)
+    # Returns the list of clients and appends self to the list if not already there
+    '''try:
+        clientsArray.index(ownPort)
+    except ValueError:
+        clientsArray.append(ownPort)
+        return clientsArray'''
+    clientsArray.append(ownPort)
+    
     return clientsArray
 
     
-# Retrieve clients clienlist
+# Retrieve clients clientlist
 @app.route("/list")
 def list():
-    return "client1,client2"
+    userlistString = "<p>"
+    for u in onlineUsers:
+        userlistString += str(u)+","
+    userlistString = userlistString[:-1] + "</p>"
+    return userlistString
     
 # Introduce client to list of clients
 @app.route("/list/<username>")
 def online(username):
     onlineUsers.append(username)
-    
     return "<p>Welcome " + username + "</p>"
 
 
--- test.py
+++ test.py
@@ -1,28 +1,20 @@
 import urllib.request
 globalVar = 0
-array = []
+array = ["client1", "client2"]
+
+def iterateArray():
+    for item in array:
+        print(str(item))
 
 def main():
     clientsString = ""
     clientsArray = []
     port = 4000
     ip = "192.168.99.100"
-    for i in range(10):
-        requestString = "http://" + ip + ":" + str(port+i) + "/list"
-        
-        print("Trying to fetch list on: " + requestString)
-        try:
-            clientsString = urllib.request.urlopen(requestString).read()
-            print(clientsString)
-            string = str(clientsString)
-            array = string[2:-1].split(",")
-            for e in array:
-                print(e)
-            if len(array) > len(clientsArray):
-                print("Updating array")
-                clientsArray = array
-        except urllib.error.URLError as e:
-            print(e.reason)
+    for i in array:
+        clientsString += str(i) + ","
+    clientsString = clientsString[:-2] + "hell"
+    print(clientsString)
 
 if __name__ == "__main__":
     main()
